5b9f8e93822a9261507c0b93f5972ee64e7fa747
vince-tiel <isabellevincent202804@gmail.com>
2024-09-25 10:53:28 +0200

    changement

--- src/ui/components/navigation/navigations.tsx
+++ src/ui/components/navigation/navigations.tsx
@@ -7,38 +7,38 @@ import { ActiveLink } from "./active-link"
 
 interface Props {}
 
-export const Navigation = ({}:Props) => {
+export const Navigation = ({ }: Props) => {
     return (
         <div className="border-b-2 border-gray-400">
             <Container className="flex items-center justify-between py-1.5 gap-7">
                 <Link href="/">
-                <div className="flex items-center gap-2.5">
-                    <Logo size="small"/>
-                    <div className="flex flex-col ">
-                        <div className="text-gray font-extrabold text-[24px]">
-                            Apprend à coder
-                        </div>
+                    <div className="flex items-center gap-2.5">
+                        <Logo size="small" />
+                        <div className="flex flex-col ">
+                            <div className="text-gray font-extrabold text-[24px]">
+                                Apprend à coder
+                            </div>
                             <Typography variant="caption4" component="span" theme="gray">
                                 Trouve de l'inspiration ici
                             </Typography>
+                        </div>
                     </div>
-                    </div>
-                    </Link>
-            <div className="flex items-center gap-7">
-                <Typography variant="caption3" component="div" className="flex items-center gap-7" >
-                   <ActiveLink href="/design-system">Design System</ActiveLink> 
-                   <ActiveLink href="/projets">Projets</ActiveLink> 
-                   <ActiveLink href="/formations">Formations</ActiveLink> 
-                   <ActiveLink href="/contact">Contact</ActiveLink> 
-                </Typography>
+                </Link>
+                <div className="flex items-center gap-7">
+                    <Typography variant="caption3" component="div" className="flex items-center gap-7" >
+                        <ActiveLink href="/design-system">Design System</ActiveLink>
+                        <ActiveLink href="/projets">Projets</ActiveLink>
+                        <ActiveLink href="/formations">Formations</ActiveLink>
+                        <ActiveLink href="/contact">Contact</ActiveLink>
+                    </Typography>
                     <div className="flex items-center gap-2" >
                         <Button size="small">Connexion</Button>
                         <Button size="small" variant="secondary" >Rejoindre</Button>
                     
                     </div>
-            </div>
+                </div>
 
             </Container>
         </div>
-    )
-}
+    );
+};
