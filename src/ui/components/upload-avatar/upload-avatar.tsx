c4907f60586d8bd0339225035fbfbb0e15a07676
vince-tiel <isabellevincent202804@gmail.com>
2024-10-21 11:01:48 +0200

    commit4-1

--- src/ui/components/upload-avatar/upload-avatar.tsx
+++ src/ui/components/upload-avatar/upload-avatar.tsx
@@ -3,8 +3,9 @@ import { RiCamera2Fill } from "react-icons/ri";
 
 interface Props {
   handleImageSelect: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  imagePreview: string | ArrayBuffer | null;
 }
-export const UploadAvatar = ({ handleImageSelect }: Props) => {
+export const UploadAvatar = ({ handleImageSelect, imagePreview }: Props) => {
   return (
     <div className="flex items-center gap-5">
       <label className="inline-block text-white bg-primary hover:bg-primary-400 rounded px-[18px] py-[15px] text-caption2 font-medium cursor-pointer animate">
@@ -14,7 +15,17 @@ export const UploadAvatar = ({ handleImageSelect }: Props) => {
         </div>
         <input type="file" className="hidden" onChange={handleImageSelect} />
       </label>
-      <Avatar size="extra-large" alt="avatar" src="/assets/svg/camera.svg" />
+      <Avatar
+        size="extra-large"
+        alt="avatar"
+        src={
+          imagePreview
+            ? typeof imagePreview === "string"
+              ? imagePreview
+              : String(imagePreview)
+            : "/assets/svg/camera.svg"
+        }
+      />
     </div>
   );
 };
--- src/ui/modules/onboarding/components/steps/avatar-step/avatar-step.tsx
+++ src/ui/modules/onboarding/components/steps/avatar-step/avatar-step.tsx
@@ -21,54 +21,65 @@ export const AvatarStep = ({
 }: BaseComponentProps) => {
   const { authUser } = useAuth();
   const { value: isLoading, setValue: setLoading } = useToggle();
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
-  const handleImageSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      setSelectedImage(file);
+    const [selectedImage, setSelectedImage] = useState<File | null>(null);
+    const [imagePreview, setImagePreview] = useState<
+      string | ArrayBuffer | null
+    >(null);
+    const handleImageSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files?.[0];
+      if (file) {
+        setSelectedImage(file);
 
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        let imgDataUrl: string | ArrayBuffer | null = null;
-        if (e.target) {
-          imgDataUrl = e.target.result;
-        }
-      };
-    }
-  };
-  return (
-    <div className="relative h-screen pb-[91px]">
-      <div className="h-full overflow-auto">
-        <Container className="grid h-full grid-cols-12">
-          <div className="relative z-10 flex items-center h-full col-span-6 py-10">
-            <div className="w-full space-y-5 pb-4.5">
-              <OnboardingTabs tabs={stepList} getCurrentStep={getCurrentStep} />
-              <Typography variant="h1" component="h1">
-                Dernière étape !
-              </Typography>
-              <Typography variant="body-base" component="p" theme="gray">
-                C'est sûr t'as une tête à être sur Coders Monkeys ! Mais on a
-                besoin de ta plus belle photo de profil pour que tout le monde
-                puisse voir à quel point tu es incroyable. Mettre une photo
-                sympa, c'est la garantie de te faire remarquer et de faire
-                craquer les développeurs en quête de nouveaux contacts. Alors
-                montre-nous ta belle gueule et rejoins la communauté !
-              </Typography>
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          let imgDataUrl: string | ArrayBuffer | null = null;
+          if (e.target) {
+            imgDataUrl = e.target.result;
+          }
+          setImagePreview(imgDataUrl);
+        };
+        reader.readAsDataURL(file);
+      }
+    };
+    return (
+      <div className="relative h-screen pb-[91px]">
+        <div className="h-full overflow-auto">
+          <Container className="grid h-full grid-cols-12">
+            <div className="relative z-10 flex items-center h-full col-span-6 py-10">
+              <div className="w-full space-y-5 pb-4.5">
+                <OnboardingTabs
+                  tabs={stepList}
+                  getCurrentStep={getCurrentStep}
+                />
+                <Typography variant="h1" component="h1">
+                  Dernière étape !
+                </Typography>
+                <Typography variant="body-base" component="p" theme="gray">
+                  C'est sûr t'as une tête à être sur Coders Monkeys ! Mais on a
+                  besoin de ta plus belle photo de profil pour que tout le monde
+                  puisse voir à quel point tu es incroyable. Mettre une photo
+                  sympa, c'est la garantie de te faire remarquer et de faire
+                  craquer les développeurs en quête de nouveaux contacts. Alors
+                  montre-nous ta belle gueule et rejoins la communauté !
+                </Typography>
+              </div>
             </div>
-          </div>
-          <div className="flex items-center h-full col-span-6">
-            <div className="flex justify-center w-full">
-              <UploadAvatar handleImageSelect={handleImageSelect} />
+            <div className="flex items-center h-full col-span-6">
+              <div className="flex justify-center w-full">
+                <UploadAvatar
+                  handleImageSelect={handleImageSelect}
+                  imagePreview={imagePreview}
+                />
+              </div>
             </div>
-          </div>
-        </Container>
+          </Container>
+        </div>
+        <OnboardingFooter
+          prev={prev}
+          next={next}
+          isFinalStep={isFinalStep}
+          isLoading={isLoading}
+        />
       </div>
-      <OnboardingFooter
-        prev={prev}
-        next={next}
-        isFinalStep={isFinalStep}
-        isLoading={isLoading}
-      />
-    </div>
-  );
+    );
 };
