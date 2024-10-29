1153311f96a53ebc1f7a33f978eea779746b9907
vince-tiel <isabellevincent202804@gmail.com>
2024-10-29 10:56:48 +0100

    commit4-6

--- src/ui/modules/user-profile/profile/profile.container.tsx
+++ src/ui/modules/user-profile/profile/profile.container.tsx
@@ -3,6 +3,9 @@ import { ProfileView } from "./profile.view";
 import { useToggle } from "@/hooks/use-toggle";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { UserProfileFormFieldsType } from "@/types/forms";
+import { useEffect } from "react";
+import { FirestoreUpdateDocument } from "@/api/firestore";
+import { toast } from "react-toastify";
 
 export const ProfileContainer = () => {
   const { authUser } = useAuth();
@@ -15,17 +18,77 @@ export const ProfileContainer = () => {
     setValue,
     setError
   } = useForm<UserProfileFormFieldsType>();
+
+  const { displayName, expertise, biography, linkedin, github } =
+    authUser.userDocument;
+  useEffect(() => {
+    const fieldsToUpdate: (
+      | "displayName"
+      | "expertise"
+      | "biography"
+      | "linkedin"
+      | "github"
+    )[] = ["displayName", "expertise", "biography", "linkedin", "github"];
+    for (const field of fieldsToUpdate) {
+      setValue(field, authUser.userDocument[field]);
+    }
+  }, []);
+
   const handleUpdateUserDocument = async (
     formData: UserProfileFormFieldsType
   ) => {
     setLoading(true);
+
+    const { error } = await FirestoreUpdateDocument(
+      "users",
+      authUser.uid,
+      formData
+    );
+
+    if (error) {
+      setLoading(false);
+      toast.error(error.message);
+      return;
+    }
+    toast.success("Ton profil a été mis à jour");
     setLoading(false);
   };
   const onSubmit: SubmitHandler<UserProfileFormFieldsType> = async (
     formData
   ) => {
-    handleUpdateUserDocument(formData);
-    return;
+    //@todo
+    if (formData.linkedin && !formData.linkedin.includes("linkedin.com/")) {
+      setError("linkedin", {
+        type: "manual",
+        message: "Cet URL ne correspond pas à un profil linkedin"
+      });
+      return;
+    }
+    if (formData.github && !formData.github.includes("github.com/")) {
+      setError("github", {
+        type: "manual",
+        message: "Cet URL ne correspond pas à un profil github"
+      });
+      return;
+    }
+    if (
+      displayName !== formData.displayName ||
+      expertise !== formData.expertise ||
+      biography !== formData.biography ||
+      linkedin !== formData.linkedin ||
+      github !== formData.github
+    ) {
+      for (const key in formData) {
+        if (
+          formData.hasOwnProperty(key) &&
+          formData[key as keyof UserProfileFormFieldsType] === undefined
+        ) {
+          delete formData[key as keyof UserProfileFormFieldsType];
+        }
+      }
+      handleUpdateUserDocument(formData);
+      return;
+    }
   };
 
   return (
